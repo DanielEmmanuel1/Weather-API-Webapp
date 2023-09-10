d4e962735e331ec5ae5c1c7ed252c7e7a5f467d4
Crypt.sol.codes <110722148+DanielEmmanuel1@users.noreply.github.com>
2023-09-10 08:55:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const fetchAPI = async (city) => {
         console.log(data)
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".humidity").innerHTML = data.main.humidity;
-        document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)}°C`;
-        document.querySelector(".wind").innerHTML = data.wind.speed;
-        document.querySelector(".cloud").innerHTML = data.weather[0].description;
+        document.querySelector(".temp").textContent = `${Math.round(data.main.temp)}°C`;
+        document.querySelector(".wind").textContent = data.wind.speed;
+        document.querySelector(".cloud").textContent = data.weather[0].description;
     }catch(error){
         console.log(`There was an error`);
     }
